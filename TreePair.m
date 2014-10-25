76801b1927985915558ae42ad84ee2c2c205f02c
NehaNayak <neha91@gmail.com>
2014-10-25 14:49:56 -0700

    treepair should work now

--- TreePair.m
+++ TreePair.m
@@ -6,43 +6,8 @@ classdef TreePair < handle
     % - The text with which the tree can be displayed.
     % - The features at the node.
     
-    properties (Hidden) %TODO: Make all private.
-	weights = [];
-    end
-    
-	methods
+	methods(Static)
 	
-	function newInd = comparePtoH(p,h,n,theta,decoder, wordFeatures, hyperParams)
-		this.compareH(p,h,n,1,theta,decoder, wordFeatures, hyperParams)
-		if isLeaf(p)
-			newInd = n+1;
-			return
-		else
-			nextInd = this.comparePtoH(p.daughters(1),h,n+1,theta,decoder, wordFeatures, hyperParams);
-			nextInd = this.comparePtoH(p.daughters(2),h,nextInd,theta,decoder, wordFeatures, hyperParams);
-			newInd = nextInd+1;
-		end
-	end
-
-	function newInd = compareH(p,h,n,i,theta,decoder, wordFeatures, hyperParams)
-		data = struct('relation', 1, 'leftTree', p.copy(), 'rightTree', h.copy(), 'id', 0, 'score', 0);
-		probsForward=ComputeRelationProbs(theta, decoder, data, wordFeatures, hyperParams);
-		data = struct('relation', 1, 'leftTree', h.copy(), 'rightTree', p.copy(), 'id', 0, 'score', 0);
-		probsReverse=ComputeRelationProbs(theta, decoder, data, wordFeatures, hyperParams);
-		%weights(n,i) = 1-0.5*(probsReverse(1)*probsForward(1));
-		this.weights(n,i) = n+i;
-	
-		if isLeaf(h)
-			newInd =  n+1;
-			return
-		else
-			nextInd = this.compareH(p,h.daughters(1),n,i+1,theta,decoder, wordFeatures, hyperParams);
-			nextInd = this.compareH(p,h.daughters(2),n,nextInd,theta,decoder, wordFeatures, hyperParams);
-			newInd =  nextInd+1;
-		end
-		
-	end
-
 	function [p,h] = makeTreePair(iTextp,iTexth,wordMap,theta,decoder, wordFeatures, hyperParams)
 		p = Tree.makeTree(iTextp,wordMap);
 		h = Tree.makeTree(iTexth,wordMap);
@@ -50,9 +15,41 @@ classdef TreePair < handle
 		pSubtreesLength = int32(size(strfind(iTextp,'('),2));
 		hSubtreesLength = int32(size(strfind(iTexth,'('),2));
 
-		this.weights = zeros(pSubtreesLength,hSubtreesLength);
-		this.comparePtoH(p,h,1,theta,decoder, wordFeatures, hyperParams)
+		weights = zeros(pSubtreesLength,hSubtreesLength);
+		weights = comparePtoH(p,h,1,theta,decoder, wordFeatures, hyperParams, weights)
 		disp(this.weights)
 	end
+
+	function [newInd, weights] = comparePtoH(p,h,n,theta,decoder, wordFeatures, hyperParams, weights)
+		this.compareH(p,h,n,1,theta,decoder, wordFeatures, hyperParams)
+		if isLeaf(p)
+			newInd = n+1;
+			return
+		else
+			nextInd = this.comparePtoH(p.daughters(1),h,n+1,theta,decoder, wordFeatures, hyperParams, weights);
+			nextInd = this.comparePtoH(p.daughters(2),h,nextInd,theta,decoder, wordFeatures, hyperParams, weights);
+			newInd = nextInd+1;
+		end
+	end
+
+	function [newInd, weights] = compareH(p,h,n,i,theta,decoder, wordFeatures, hyperParams, weights)
+		data = struct('relation', 1, 'leftTree', p.copy(), 'rightTree', h.copy(), 'id', 0, 'score', 0);
+		probsForward=ComputeRelationProbs(theta, decoder, data, wordFeatures, hyperParams);
+		data = struct('relation', 1, 'leftTree', h.copy(), 'rightTree', p.copy(), 'id', 0, 'score', 0);
+		probsReverse=ComputeRelationProbs(theta, decoder, data, wordFeatures, hyperParams);
+		%weights(n,i) = 1-0.5*(probsReverse(1)*probsForward(1));
+		weights(n,i) = n+i;
+	
+		if isLeaf(h)
+			newInd =  n+1;
+			return
+		else
+			nextInd = this.compareH(p,h.daughters(1),n,i+1,theta,decoder, wordFeatures, hyperParams, weights);
+			nextInd = this.compareH(p,h.daughters(2),n,nextInd,theta,decoder, wordFeatures, hyperParams,weights);
+			newInd =  nextInd+1;
+		end
+		
+	end
+
     end
 end
